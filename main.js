0eb9ef085150b4409bfabb6985d7c04c437c076d
gerardolsj <lsjgerardo@gmail.com>
2016-07-05 15:42:14 -0500

    snapshot()

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-
-
-
-$('#fire').submit(function() {
-   var $inputs = $('#fire :input');
-console.log("exito:"+name);
-   var name = $('#email').val();
-   var pass = $('psw').val();
-   console.log("exito:"+name);
-
-});
-
 function writeUserData(userId, name, email) {
   firebase.database().ref('users/' + userId).set({
     username: name,
@@ -22,3 +10,36 @@ function pushUserData( name, email) {
     email: email
   });
 }
+/*    var users = [];
+    firebase.database().ref('users').on('value', function(snapshot) {
+    users(snapshot.val());
+    console.log(users);
+    });
+*/
+var data = [];
+firebase.database().ref('users').on('value', function(snapshot) { 
+    data = (snapshot.val());
+
+    data.username.forEach(function(element) {
+         $("#list").append('<li></li>');
+        
+    });
+})
+
+
+
+
+
+$(document).ready(function(){
+    $('#fire').on('submit', function(e){
+        e.preventDefault();
+        var name = $('#name').val();
+        var pass = $('#pwd').val();
+        pushUserData( name, pass);
+        console.log("Pushed");
+        document.getElementById("fire").reset();
+       
+
+    });
+});
+

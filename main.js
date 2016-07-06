d68415e06fca90403b21d2f00e2759588a736c21
gerardolsj <lsjgerardo@gmail.com>
2016-07-06 16:13:21 -0500

    angular

--- main.js
+++ main.js
@@ -1,54 +1,51 @@
-function writeUserData(userId, name, email) {
-  firebase.database().ref('users/' + userId).set({
-    username: name,
-    email: email
-  });
-}
-function pushUserData( name, email) {
-  firebase.database().ref('users/').push({
-    username: name,
-    email: email
-  });
-}
-/*    var users = [];
-    firebase.database().ref('users').on('value', function(snapshot) {
-    users(snapshot.val());
-    console.log(users);
-    });
-*/
-var obj;
-var result;
-firebase.database().ref('users').on('value', function(snapshot) { 
-    obj = (snapshot.val());
-    var arr = [];
-    for (var key in obj) {
-        if (obj.hasOwnProperty(key)) {
-            arr.push(obj[key].username);
-        }
-    };
-    result = arr;
-     for(var i in result){
-           $("#list").append('<li>'+result[i]+'</li>');
-       
-      }
-})
+var app = angular.module("sampleApp", ["firebase"]);
 
+app.controller("SampleCtrl", function($scope, $firebaseArray) {
+  var ref = firebase.database().ref().child("messages");
+  // create a synchronized array
+  $scope.messages = $firebaseArray(ref);
+  // add new items to the array
+  // the message is automatically added to our Firebase database!
+  $scope.addMessage = function() {
+    $scope.messages.$add({
+      text: $scope.newMessageText
+    });
+  };
+  // click on `index.html` above to see $remove() and $save() in action
+});
+
+
+
+
+/*
+
+function pushUserData( name, text) {
+  firebase.database().ref('messages').push({
+    name: name,
+    text: text
+  });
+}
+
+ var setMessage = function(data) {
+    var val = data.val();
+   // displayMessage(data.key, val.name, val.text, val.photoUrl, val.imageUrl);
+   $("#list").append('<li>'+val.name+ ': '+val.text+'</li>');
+   console.log(val.text);
+ }
+
+firebase.database().ref('messages').on('child_added', setMessage);
 
 
 $(document).ready(function(){
     $('#fire').on('submit', function(e){
         e.preventDefault();
+        //this.name 
         var name = $('#name').val();
         var pass = $('#pwd').val();
-        pushUserData( name, pass);
-        console.log("Pushed");
+        pushUserData(name, pass);
+        //console.log("Pushed");
         document.getElementById("fire").reset();
-     
-             
-            
-       
-      
 
     });
 });
-
+*/
